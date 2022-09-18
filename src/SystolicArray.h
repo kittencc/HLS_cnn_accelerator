6a451fa1f04cdbc8871be93659d9b3e77685ba52
Christopher Torng <c.c2issy@gmail.com>
2022-09-18 14:54:17 -0700

    upload catapult manual

--- src/SystolicArray.h
+++ src/SystolicArray.h
@@ -24,7 +24,32 @@ void run(ac_channel<Params> &paramsIn,
         // Generate the loop indices here for the systolic array.
         // Write the loop indices as well as the params out to channels.
         // -------------------------------
-        
+        Params params = paramsIn.read();
+
+        uint_16 ofmap_bank_num = params.OY1 * params.OX1 * params.OC1;
+
+        #pragma hls_unroll no
+        LABEL(OY1_OX1_OC1) for (uint_16 idx = 0; idx < ofmap_bank_num; idx ++ ) {
+          #pragma hls_unroll no
+          LABEL(IC1) for (uint_16 ic1 = 0; ic1 < params.IC1; ic1++){
+            #pragma hls_unroll no
+            LABEL(FY) for (uint_16 fy = 0; fy < params.FY; fy++){
+              #pragma hls_unroll no
+              LABEL(FX) for (uint_16 fx = 0; fy < params.FX; fx++){
+
+                paramsOut.write(params);
+
+                LoopIndices loopIndices = {
+                  ic1,
+                  fy,
+                  fx
+                };
+
+                loopIndicesOut.write(loopIndices);
+              }
+            }
+          }
+        }
         // -------------------------------
         // Your code ends here
         // -------------------------------
@@ -36,11 +61,11 @@ class SystolicArrayWrapper
 {
 public:
     SystolicArrayWrapper(){}
-    
+
 #pragma hls_design interface
 #pragma hls_pipeline_init_interval 1
-    void run(ac_channel<PackedInt<INPUT_PRECISION, IC0> > &input, 
-             ac_channel<PackedInt<WEIGHT_PRECISION, OC0> > &weight, 
+    void run(ac_channel<PackedInt<INPUT_PRECISION, IC0> > &input,
+             ac_channel<PackedInt<WEIGHT_PRECISION, OC0> > &weight,
              ac_channel<PackedInt<OUTPUT_PRECISION, OC0> > &output,
              ac_channel<Params> &paramsIn)
     {
